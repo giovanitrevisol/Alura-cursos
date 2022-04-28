4a7ae7c22474776bf0f8a7412e39f750d5ca7b32
Giovani Trevisol <giovanitrevisol93@gmail.com>
2022-04-27 23:17:18 -0300

    create project forum

---
+++ forum/src/test/java/br/com/gt/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gt.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
